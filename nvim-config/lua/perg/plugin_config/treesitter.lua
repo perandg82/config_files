bcbc365d1faaa0820599d5b21ea1767333b0f885
Per Andreas Gulbrandsen <per.andreas.gulbrandsen@nomono.co>
2023-10-20 15:00:22 +0200

    updated config

--- nvim-config/lua/perg/plugin_config/treesitter.lua
+++ nvim-config/lua/perg/plugin_config/treesitter.lua
@@ -1,5 +1,18 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = {"c", "lua", "rust", "ruby", "vim", "python" },
+	ensure_installed = {
+		"c",
+		"lua",
+		"rust",
+		"ruby",
+		"vim",
+		"python",
+		"gitignore",
+		"json",
+		"yaml",
+		"bash",
+		"zsh",
+		"dockerfile",
+	},
 	sync_install = false,
 	auto_install = true,
 	highlight = {
