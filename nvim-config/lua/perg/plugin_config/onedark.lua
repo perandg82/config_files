a5284b0cabb5165cf77cae7341a55d1b5ca7cf01
Per Andreas Gulbrandsen <peran.gulbrandsen@zolve.no>
2023-12-18 10:38:11 +0100

    update to colorscheme mod

--- nvim-config/lua/perg/plugin_config/onedark.lua
+++ nvim-config/lua/perg/plugin_config/onedark.lua
@@ -1,8 +1,8 @@
 local cfg = vim.g.onedark_config
 require('onedark').setup {
     style = 'deep',
-	highlights = {
-		["@comment"] = {fg = "#6c7d9c"},
-	}
+    colors = {
+	grey = "#6c7d9c",
+    },
 }
 require('onedark').load()
