eed67ad1dba4e1f3666fd713c905763ede8a123d
Per Andreas Gulbrandsen <peran.gulbrandsen@zolve.no>
2024-02-08 10:14:33 +0100

    test indent lines

--- nvim-config/lua/perg/plugins.lua
+++ nvim-config/lua/perg/plugins.lua
@@ -49,6 +49,10 @@ return require('packer').startup(function(use)
 	  requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'rafamadriz/friendly-snippets', },
   }
   use {
+	  "lukas-reineke/indent-blankline.nvim",
+	  require('ibl').setup()
+  }
+  use {
 	  "williamboman/mason.nvim",
 	  "williamboman/mason-lspconfig.nvim",
 	  "neovim/nvim-lspconfig",
